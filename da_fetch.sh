18009070559c21727150065345917ed7a7babf56
Felix Hemme <hemme.felix@mailbox.org>
2022-08-29 08:27:00 +0200

    expansion and adaptation to new mapping

---
+++ da_fetch.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+shopt -s nocasematch;
+
+# Variables
+file=${1}
+fileUnix="${file}.unix.txt"
+defaultTarget="k10plus"
+target="${2:-$defaultTarget}"
+isil=${3}
+format="marcxml-solr"
+
+display_usage() {
+    echo -e "\nDas Programm erwartet drei Argumente beim Aufruf:\n./da_fetch.sh [Dateiname] [Target] [ISIL]\\n
+Sofern kein Target angegeben wird, werden die Daten aus dem K10plus abgezogen.\\n
+Sofern kein ISIL angegeben wird, bleibt die Spalte identifier.packageid leer.\n"
+}
+
+display_targets() {
+	echo -e "Folgende Targets sind verfügbar:\n\
+- k10plus (Datenbank 1.1)\n\
+- ebooks (Datenbank 1.2)\n\
+- nl-monographien (Datenbank 1.50)\n\
+- nl-zeitschriften (Datenbank 1.55)\n"
+}
+
+# Check on arguments
+if [[ $# -eq 0 ]] ; then
+    echo -e "\nFEHLER: Kein Dateiname angegeben."
+    display_usage
+	display_targets
+    exit 1
+fi
+
+# Validate target argument
+if [[ $target != "k10plus" && $target != "ebooks" && $target != "nl-monographien" && $target != "nl-zeitschriften" ]] ; then
+	echo -e "Ungültiges Target.\n"
+	display_targets
+	exit 1
+fi
+
+if [[ $target = "k10plus" ]] ; then
+	echo -e "Daten werden aus Target \"k10plus\" abgezogen."
+elif [[ $target = "ebooks" ]] ; then
+	echo -e "Daten werden aus Target \"ebooks\" abgezogen."
+elif [[ $target = "nl-monographien" ]] ; then
+	echo -e "Daten werden aus Target \"nl-monographien\" abgezogen."
+elif [[ $target = "nl-zeitschriften" ]] ; then
+	echo -e "Daten werden aus Target \"nl-zeitschriften\" abgezogen."
+fi
+
+if [[ $isil = "zdb-33-sfen" ]] ; then
+	echo -e "Wert in Spalte identifier.packageid = \"ZDB-33-SFEN\""
+fi
+
+echo -e "Datei \"$file\" wird verarbeitet."
+
+awk '{ sub("\r$", ""); print }' < ${1} > ${fileUnix}
+
+echo -e "Bitte warten. Datensätze werden heruntergeladen."
+
+# Schnittstelleninformationen
+cat ${fileUnix} | xargs -n 1 -i curl -s "http://unapi.k10plus.de/?id=${target}:ppn:{}&format=${format}" > records.xml
+
+if [[ -s records.xml ]]
+then
+    echo -e "Download erfolgreich.";
+else
+    echo -e "Download fehlgeschlagen, Programm bricht ab.";
+    exit 1
+fi
+
+echo -e "Bitte warten. Heruntergeladene Datensätze werden konvertiert."
+
+catmandu convert MARC --type XML to CSV --fix Version_kram_marc2csv-solr-dev.fix --fields identifier.ppn,type,date.issued,title,part,\
+title.alternative,identifier.isbn,relation.issn,relation.journalzdbid,relation.serieszdbid,contributor.author,contributor.editor,contributor.other,identifier,\
+identifier.pi,rights,rights.license,publisher,language.iso,subject.jel,description.version,relation.ispartofseries,relation.seriesppn,\
+relation.ispartofjournal,relation.journalppn,relation.ispartofbook,relation.bookppn,econstor.citation.volume,econstor.citation.issue,econstor.citation.articlenumber,\
+econstor.citation.startpage,econstor.citation.endpage,url,collection_handle,identifier.packageid,filepath,description.abstract,subject.ddc,subject.keyword --var target=${target} --var isil=${isil} --sep_char '\t' < records.xml > records-${1}.csv
+
+if [[ -s records-${1}.csv ]]
+then
+    echo -e "Konvertierung erfolgreich.";
+else
+    echo -e "Konvertierung fehlgeschlagen, Programm bricht ab.";
+    exit 1
+fi
+
+ppnDir="archive/ppns"
+recordsDir="archive/records"
+
+[ ! -d "$ppnDir" ] && mkdir -p "$ppnDir"
+[ ! -d "$recordsDir" ] && mkdir -p "$recordsDir"
+
+mv *.txt archive/ppns
+mv records-* archive/records
+
+rm records.xml
